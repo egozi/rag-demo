20fa4b3826c020ffda4729f854a438f79137b0e6
Amir Egozi <egozi5@gmail.com>
2026-06-22 18:42:55 +0300

    fix logging

--- api/main.py
+++ api/main.py
@@ -1,5 +1,12 @@
+import logging
 import gradio as gr
 from fastapi import FastAPI
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s | %(message)s",
+    datefmt="%H:%M:%S",
+)
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
